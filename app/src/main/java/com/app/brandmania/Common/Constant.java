bde42fa34e58911ecc4a9b59e2c81e85975bb637
lakharasachin-qf <lakharasachin.qf@gmail.com>
2020-12-20 14:41:14 +0530

    -Version 2 -all bugs figure out

--- app/src/main/java/com/app/brandmania/Common/Constant.java
+++ app/src/main/java/com/app/brandmania/Common/Constant.java
@@ -2,19 +2,28 @@ package com.app.brandmania.Common;
 
 public class Constant {
     public static final String MOBILE_NUMBER = "mobileNumber";
+
     public static final String EMAIL_ID = "emailid";
+
     public static final int STUDENT = 1;
+
     public static final int TEACHER = 2;
+
     public static final int ADMIN_USER = 3;
+
     public static final int SUBADMIN_USER = 4;
+
     public static final int PICKER_FIRST = 1002;
+
     public static final int SCHOOL_ID = 1;
+
     public static final int PICKER_PROFILE = 1000;
+
     public static final int GalleryRights = 9;
 
     public static final int APP_UPDATES = 1001;
-    public static final String F_VERSION = "2.0.1";
 
+    public static final String F_VERSION = "1.0.1";
 
     public static final String ADMIN_CONTACT_NUMBER="8460638464";
 
@@ -25,6 +34,5 @@ public class Constant {
     public static final boolean is_DEVELOPMENT_MODE = false;
 
     public static final int DAILY_REMINDER_REQUEST_CODE = 222;
-
 
 }
